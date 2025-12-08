1223a89ad62995d99f23921137eb78399c465da6
Goff120 <geoffreysmithuk@icloud.com>
2025-12-08 23:23:31 +0000

    understanding first areas

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //just a library thing
+
+using namespace std; 
+//use all the standard stuff in c++
 
 int main() {
-    cout << "HELLO WORLD!!" << endl;
+    cout << "HELLO WORLD!!" << endl << "the start of miko";
+    // print to console   //<< is like "," in python
     return 0;
+    //tells program that everything went fine
 }
