fc76cf435a2459cb4c9f6853c28b7935c34b7a44
Goff120 <geoffreysmithuk@icloud.com>
2025-12-08 23:30:54 +0000

    get inputs

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream> //just a library thing
+
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+    int a; 
+    int b;
+    int sum;
+    
+    //c out/in out in
+    cout << "enter your number! :\n";
+    cin >> a;
+
+    cout << "eneter another number! :\n";
+    cin >> b;
+    // arrows face way of infomation movement
+
+    sum = a + b;
+
+    cout << "you got " << sum << endl;
+
+
+    return 0;
+}
