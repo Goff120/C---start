f97ff630c3217140502a76a23ae467e42dbb440f
Goff120 <geoffreysmithuk@icloud.com>
2025-12-08 23:23:59 +0000

    add small calculator

---
+++ varables.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //just a library thing
+
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+    int a = 10;
+    int b = 7;
+    cout << a*b << endl;
+    cout << a-b << endl;
+    cout << a+b << endl;
+    cout << a/b << endl;
+    return 0;
+}
