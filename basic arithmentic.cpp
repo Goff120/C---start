d33c15c77b2264c2240550ef5f01e78e89d9b29c
Goff120 <geoffreysmithuk@icloud.com>
2025-12-09 16:39:09 +0000

    if statments test

--- basic arithmentic.cpp
+++ basic arithmentic.cpp
@@ -4,13 +4,15 @@ using namespace std;
 //use all the standard stuff in c++
 
 int main() {
-    int y = 2;
-    float x = 81/y;
+    
+    int num1 = 3;
+    int num2 = 3;
 
-    int test1 = 4*3+2*4;
-    int test2 = 4*(3+2)*4;
-
-    cout << test1 << endl << test2;
+    if (num1 == num2){
+        cout << "jeff the land shark is the best";
+    } else {
+        cout << "jeff's ult is not that great";
+    }
 
     return 0;
 }
