1c7577759e54dc6bd0b654ef2c55e42c86a5afbb
Goff120 <geoffreysmithuk@icloud.com>
2025-12-09 16:35:01 +0000

    basic maths are true

---
+++ basic arithmentic.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //just a library thing
+
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+    int y = 2;
+    float x = 81/y;
+
+    int test1 = 4*3+2*4;
+    int test2 = 4*(3+2)*4;
+
+    cout << test1 << endl << test2;
+
+    return 0;
+}
