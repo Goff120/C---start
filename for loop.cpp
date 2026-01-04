9a89d9e42b19b2ebf77dcb6140ac9d1e99511ed8
Goff120 <geoffreysmithuk@icloud.com>
2026-01-04 22:28:01 +0000

    wooo for loop king is back

---
+++ for loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //just a library thing
+
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+    //for loop is king ヾ(≧▽≦*)o
+    //  start  how it ends   by how much
+    for(int x = 20; x <= 40; x+= 5) {
+        cout << x << endl;
+    }
+
+}
