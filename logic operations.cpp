61d3613479fc7304eef0d9e149939c11cba88c1c
Goff120 <geoffreysmithuk@icloud.com>
2026-01-05 14:49:23 +0000

    logic operations

---
+++ logic operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //just a library thing
+#include <cmath>
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+
+    int damage = 320;
+    int rank = 2;
+                //And in python
+    if (damage >200 && rank == 2){
+        cout << "you can evolve rookie" << endl;
+    } else {
+        cout << "you need more power my guy" << endl;
+    }
+}
