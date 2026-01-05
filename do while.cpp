35ca082e36f096afecad12ca72c50ec1100a773f
Goff120 <geoffreysmithuk@icloud.com>
2026-01-05 14:30:26 +0000

    a do while loop

---
+++ do while.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //just a library thing
+#include <cmath>
+using namespace std; 
+//use all the standard stuff in c++
+
+int main() {
+
+    int mon = 13;
+
+    do{
+        cout << "you have " << mon << " digimons" << endl;
+        mon++;
+    }while (mon < 10);
+}
+//this does the code 1 time then test 
+//so if i have a value of 12 it will still print 
